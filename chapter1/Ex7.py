5bd402678757bbca47cebd93e563418fa41a03ec
Hapbi <seashihoxd0987@Gmail.com>
2026-01-30 09:29:23 +0700

    Update Ex7.py

--- chapter1/Ex7.py
+++ chapter1/Ex7.py
@@ -1,1 +1,10 @@
+a = float(input("nhâp số nguyên đầu tiên bất kỳ: "))
+b = float(input("nhâp số nguyên đầu thứ hai bất kỳ: "))
+c = float(input("nhâp số nguyên đầu thứ ba bất kỳ: "))
+
+max = max(a , b ,c)
+min = min(a, b, c)
+
+print("Số lớn nhất là: ",max) 
+print("Sô bé nhất là: ",min)
 
