6ed0b6efc8e728b146b5a13ef408adb201a332e9
Hapbi <seashihoxd0987@Gmail.com>
2026-02-01 22:10:14 +0700

    Update Ex14.py

--- chapter1/Ex14.py
+++ chapter1/Ex14.py
@@ -1,1 +1,8 @@
+import math
+n = float(input("Nhập một số: "))
+if n < 0:
+    print("Lỗi: Không thể tính căn bậc hai của số âm!")
+else:
+    can_bac_hai = math.sqrt(n)
+    print(f"Căn bậc hai của {n} là: {can_bac_hai:.2f}")
 
