8cf687fe22b08d1f7ddeaeada55443a42cd0cba2
Hapbi <seashihoxd0987@Gmail.com>
2026-03-04 21:02:08 +0700

    Update EX6.py

--- chapter 3/EX6.py
+++ chapter 3/EX6.py
@@ -1,1 +1,9 @@
-
+danhsach = list(map(int, input("Nhập danh sách số nguyên: ").split()))
+swap_count = 0
+for i in range(len(danhsach)):
+    for j in range(0, len(danhsach) - i - 1):
+        if danhsach[j] > danhsach[j + 1]:
+            danhsach[j], danhsach[j + 1] = danhsach[j + 1], danhsach[j]
+            swap_count += 1
+print("Danh sách sau khi sắp xếp:", danhsach)
+print("Số lần hoán đổi:", swap_count)
