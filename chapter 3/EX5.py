592f815fc8d38496479e262b2061a46473b2b4d1
Hapbi <seashihoxd0987@Gmail.com>
2026-03-04 21:00:05 +0700

    Update EX5.py

--- chapter 3/EX5.py
+++ chapter 3/EX5.py
@@ -1,1 +1,9 @@
-
+danhsach = list(map(float, input("Nhập danh sách số thực: ").split()))
+for i in range(1, len(danhsach)):
+    key = danhsach[i]
+    j = i - 1
+    while j >= 0 and danhsach[j] < key:
+        lst[j + 1] = danhsach[j]
+        j -= 1
+    danhsach[j + 1] = key
+print("Danh sách sau khi sắp xếp giảm dần:", danhsach)
