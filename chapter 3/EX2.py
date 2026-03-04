23ed8bf4cc064bb6027d1cead6cb7850eb173cd6
Hapbi <seashihoxd0987@Gmail.com>
2026-03-04 10:49:42 +0700

    Create EX2.py

---
+++ chapter 3/EX2.py
@@ -0,0 +1,6 @@
+danh_sach = ["An", "Bình", "Chi"]
+print("Danh sách ban đầu:", danh_sach)
+danh_sach.append("Dũng")
+print("Sau khi thêm bạn mới:", danh_sach)
+danh_sach.pop(1)
+print("Sau khi xóa bạn ở vị trí thứ hai:", danh_sach)
