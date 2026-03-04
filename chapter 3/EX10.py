aab73027ca30c3ba17f352a451ba43cffc52e28b
Hapbi <seashihoxd0987@Gmail.com>
2026-03-04 21:06:31 +0700

    Update EX10.py

--- chapter 3/EX10.py
+++ chapter 3/EX10.py
@@ -1,1 +1,4 @@
-
+danhsach = list(map(int, input("Nhập danh sách số: ").split()))
+even_numbers = [num for num in danhsach if num % 2 == 0]
+print("Các số chẵn:", even_numbers)
+print("Tổng các số chẵn:", sum(even_numbers))
