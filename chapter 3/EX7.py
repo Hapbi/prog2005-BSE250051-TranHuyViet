3344b55c8456f914922428bb401862e214abf334
Hapbi <seashihoxd0987@Gmail.com>
2026-03-04 21:02:29 +0700

    Update EX7.py

--- chapter 3/EX7.py
+++ chapter 3/EX7.py
@@ -1,1 +1,11 @@
-
+danhsach = list(map(int, input("Nhập danh sách số: ").split()))
+x = int(input("Nhập số cần tìm: "))
+found = -1
+for i in range(len(danhsach)):
+    if danhsach[i] == x:
+        found = i
+        break
+if found != -1:
+    print("Chỉ số của", x, "là:", found)
+else:
+    print(x, "không có trong danh sách")
