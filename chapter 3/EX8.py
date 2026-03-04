23066aaf2439de71e3354f14ea8167f6dcda913e
Hapbi <seashihoxd0987@Gmail.com>
2026-03-04 21:05:40 +0700

    Update EX8.py

--- chapter 3/EX8.py
+++ chapter 3/EX8.py
@@ -1,1 +1,10 @@
-
+danhsach = list(map(int, input("Nhập danh sách số: ").split()))
+found = None
+for num in danhsach:
+    if num > 10:
+        found = num
+        break
+if found is not None:
+    print("Số đầu tiên lớn hơn 10 là:", found)
+else:
+    print("Không có số nào lớn hơn 10")
