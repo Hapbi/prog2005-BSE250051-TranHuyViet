337fdca9fd7fa730e6bd3e274e60b01ed9621642
Hapbi <seashihoxd0987@Gmail.com>
2026-03-04 21:15:05 +0700

    Update EX15.py

--- chapter 3/EX15.py
+++ chapter 3/EX15.py
@@ -1,1 +1,12 @@
+#dùng kỹ thuật Slcing
+s = input("Nhập chuỗi: ")
+print("Chuỗi đảo ngược (slicing):", s[::-1])
+
+#không dùng kỹ thuật Slicing 
+s = input("Nhập chuỗi: ")
+reversed_s = ""
+for ch in s:
+    reversed_s = ch + reversed_s
+print("Chuỗi đảo ngược (không slicing):", reversed_s)
+
 
