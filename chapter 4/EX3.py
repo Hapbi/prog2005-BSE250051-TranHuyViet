2616e23690f63a90c94757882b6dfcba38112dc3
Hapbi <seashihoxd0987@Gmail.com>
2026-03-14 19:33:50 -0700

    Update EX3.py

--- chapter 4/EX3.py
+++ chapter 4/EX3.py
@@ -1,1 +1,9 @@
+def kiem_tra_key(d, key):
+    if key in d:
+        print(f"Key '{key}' tồn tại trong dictionary.")
+    else:
+        print(f"Key '{key}' KHÔNG tồn tại trong dictionary.")
+sinh_vien = {"Bốp": 9, "An": 8.5, "Bình": 7.5}
+kiem_tra_key(sinh_vien, "An")
+kiem_tra_key(sinh_vien, "Chi")
 
