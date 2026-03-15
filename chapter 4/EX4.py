4b46aaf6d37eddd0ae4360b39d2044c9121b00ff
Hapbi <seashihoxd0987@Gmail.com>
2026-03-14 19:36:55 -0700

    Update EX4.py

--- chapter 4/EX4.py
+++ chapter 4/EX4.py
@@ -1,1 +1,12 @@
+class Hoa:
+    def __init__(self, ten, mau):
+        self.ten = ten
+        self.mau = mau
+
+    def __str__(self):
+        return f"Hoa: {self.ten}, Màu: {self.mau}"
+      
+hoa1 = Hoa("Hồng", "Đỏ")
+
+print(hoa1)
 
